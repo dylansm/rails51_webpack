c7e9cbf592ce2e90b9445a10f465452923410510
Dylan Smith <dylan@rh.izo.me>
2017-09-27 17:19:04 -0700

    Up and running

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'admin/index'
+
+  devise_for :admins
   get 'welcome/index'
+  resources :admin
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
